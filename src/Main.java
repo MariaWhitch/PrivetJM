43131eb1ee98efefbfd7e63748d1e019241b1452
@MariaWhitch <dontworrybehappymary@gmail.com>
2021-10-04 23:58:58 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String[] expression = Service.readFromConsole(); // получили с консоли данные
+        boolean isArabic = Service.check(expression); // проверяем
+        String result = Service.calculate(isArabic, expression);
+        System.out.println(result);
+    }
+}
